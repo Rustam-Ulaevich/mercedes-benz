e62d476002cbde5318d43c3e356915d9c8170f37
Rustam-Ulaevich <praporglamur@gmail.com>
2023-10-29 21:27:05 +0500

    section design completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $('.design-slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    waribeleWidth: true,
+    prevArrow: '<img class="arrow__arrow-left" src="images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="arrow__arrow-rigth" src="images/arrow-rigth.svg" alt="">'
+  });
 });
