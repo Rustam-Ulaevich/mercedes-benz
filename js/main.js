33046d1e9bd49a4d6e9b6f2cd3473d8f2459fd6e
Rustam-Ulaevich <praporglamur@gmail.com>
2023-10-30 13:13:54 +0500

    adaptive layout is performed

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ $(function(){
     dots: false,
     slidesToShow: 4,
     waribeleWidth: true,
-    prevArrow: '<img class="arrow__arrow-left" src="images/arrow-left.svg" alt="">',
-    nextArrow: '<img class="arrow__arrow-rigth" src="images/arrow-rigth.svg" alt="">'
+    prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="">',
+    responsive: [
+      {
+        breakpoint: 361,
+        setting: {
+          variableWidth: false,
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 });
